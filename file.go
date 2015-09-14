8b13bd228d2151bffb6129ed961d029e12e015f5
oblank <dyh1919@gmail.com>
2015-09-14 11:52:35 +0800

    update

---
+++ file.go
@@ -0,0 +1,51 @@
+package util
+
+import (
+	"os"
+	"regexp"
+	"strings"
+)
+
+// JsonpToJson modify jsonp string to json string
+// Example: forbar({a:"1",b:2}) to {"a":"1","b":2}
+func JsonpToJson(json string) string {
+	start := strings.Index(json, "{")
+	end := strings.LastIndex(json, "}")
+	start1 := strings.Index(json, "[")
+	if start1 > 0 && start > start1 {
+		start = start1
+		end = strings.LastIndex(json, "]")
+	}
+	if end > start && end != -1 && start != -1 {
+		json = json[start : end+1]
+	}
+	json = strings.Replace(json, "\\'", "", -1)
+	regDetail, _ := regexp.Compile("([^\\s\\:\\{\\,\\d\"]+|[a-z][a-z\\d]*)\\s*\\:")
+	return regDetail.ReplaceAllString(json, "\"$1\":")
+}
+
+// The GetWDPath gets the work directory path.
+func GetWDPath() string {
+	wd := os.Getenv("GOPATH")
+	return wd
+}
+
+// The IsDirExists judges path is directory or not.
+func IsDirExists(path string) bool {
+	fi, err := os.Stat(path)
+	if err != nil {
+		return os.IsExist(err)
+	} else {
+		return fi.IsDir()
+	}
+}
+
+// The IsFileExists judges path is file or not.
+func IsFileExists(path string) bool {
+	fi, err := os.Stat(path)
+	if err != nil {
+		return os.IsExist(err)
+	} else {
+		return !fi.IsDir()
+	}
+}
--- util.go
+++ util.go
@@ -3,9 +3,18 @@ package util
 import (
 	"crypto/md5"
 	"encoding/hex"
+	"regexp"
+	"sort"
 	"strconv"
+	"strings"
 	"time"
 )
+
+// The IsNum judges string is number or not.
+func StringIsNumber(a string) bool {
+	reg, _ := regexp.Compile("^\\d+$")
+	return reg.MatchString(a)
+}
 
 //date
 func Md5String(str string) string {
@@ -36,4 +45,67 @@ func TimestampToDate(timestamp int64) string {
 func DateToTimestamp(date string, dateformat string) int64 {
 	tm, _ := time.Parse(dateformat, date)
 	return tm.Unix()
+}
+
+//range like php range
+func RangeNumber(start int, end int) []int {
+	var ret []int
+	if start <= end {
+		len := end - start
+		value := start
+		for i := 0; i <= len; i++ {
+			ret = append(ret, value)
+			value++
+		}
+	} else {
+		//start > end
+		len := start - end
+		value := start
+		for i := 0; i <= len; i++ {
+			ret = append(ret, value)
+			value--
+		}
+	}
+	return ret
+}
+
+func RangeString(sort_slice []string, start string, end string) []string {
+	sort.Strings(sort_slice)
+	startIndex := sort.SearchStrings(sort_slice, start)
+	endIndex := sort.SearchStrings(sort_slice, end)
+	if (startIndex == 0 && start != sort_slice[0]) || (endIndex == 0 && end != sort_slice[0]) {
+		return []string{}
+	}
+
+	if startIndex < endIndex {
+		return sort_slice[startIndex : endIndex+1]
+	}
+
+	if startIndex == endIndex {
+		return sort_slice[startIndex : endIndex+1]
+	}
+
+	return sort_slice[endIndex : startIndex+1]
+}
+
+func Rangeabc(start string, end string) []string {
+	if len(start) > 1 || len(end) > 1 {
+		return []string{}
+	}
+	start = strings.ToLower(start)
+	end = strings.ToLower(end)
+	alphabets := []string{"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"}
+
+	return RangeString(alphabets, start, end)
+}
+
+func RangeABC(start string, end string) []string {
+	if len(start) > 1 || len(end) > 1 {
+		return []string{}
+	}
+	start = strings.ToUpper(start)
+	end = strings.ToUpper(end)
+	alphabets := []string{"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"}
+
+	return RangeString(alphabets, start, end)
 }
