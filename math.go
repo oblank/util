e277cc1f9bf2a74cc8258102ba59c5cdbbb1bccb
oblank <dyh1919@gmail.com>
2015-10-08 15:54:44 +0800

    round

---
+++ math.go
@@ -0,0 +1,21 @@
+package util
+
+import (
+	"math"
+)
+
+//log.Println(Round(123.555555, .5, 3))
+//log.Println(Round(123.558, 0, 0))
+func Round(val float64, roundOn float64, places int) (newVal float64) {
+	var round float64
+	pow := math.Pow(10, float64(places))
+	digit := pow * val
+	_, div := math.Modf(digit)
+	if div >= roundOn {
+		round = math.Ceil(digit)
+	} else {
+		round = math.Floor(digit)
+	}
+	newVal = round / pow
+	return
+}
